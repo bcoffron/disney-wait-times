c7a5ea6db1547c37b9febe252a8f148886ff4ad2
bcoffron <bcoffron@lunchboxdad.com>
2026-05-07 14:26:19 -0500

    Add maxDuration config export to reoptimize

--- api/reoptimize.js
+++ api/reoptimize.js
@@ -1,3 +1,5 @@
+module.exports.config = { maxDuration: 60 };
+
 module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
