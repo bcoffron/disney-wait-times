fd8afb6c0e23839e3a16c795641bf1491a226ac1
bcoffron <bcoffron@lunchboxdad.com>
2026-06-03 09:47:59 -0500

    fix: convert blog-auth.js from CJS to ESM

--- api/blog-auth.js
+++ api/blog-auth.js
@@ -1,7 +1,7 @@
 // api/blog-auth.js - POST /api/blog-auth - 5 attempts/IP/hour rate limit
-const jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken';
 const attempts = {};
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
