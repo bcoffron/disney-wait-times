c9364fac4704cfc919cc1227add10b69c7a76e23
bcoffron <bcoffron@lunchboxdad.com>
2026-06-05 13:11:28 -0500

    Fix: Update New Post buttons to call newPost() instead of openNewPost()

--- api/blog-admin.js
+++ api/blog-admin.js
@@ -55,7 +55,7 @@ const ADMIN_HTML = `<!DOCTYPE html>
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"/><\/svg>
 Live Posts <span class="nav-badge" id="posts-badge">0<\/span>
 <\/button>
-<button class="nav-item" id="nav-new" onclick="openNewPost()">
+<button class="nav-item" id="nav-new" onclick="newPost()">
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125"/><\/svg>
 New Post
 <\/button>
@@ -87,7 +87,7 @@ Batch Upload
 <div id="posts-view">
 <div class="posts-header">
 <h1 class="posts-title">Live Posts<\/h1>
-<button class="btn-new-post" onclick="openNewPost()">+ New Post<\/button>
+<button class="btn-new-post" onclick="newPost()">+ New Post<\/button>
 <\/div>
 <input type="text" class="search-input" placeholder="Search posts..." oninput="filterPosts(this.value)">
 <div class="posts-table" id="posts-table"><\/div>
@@ -97,7 +97,7 @@ Batch Upload
 <div id="drafts-view" style="display:none">
 <div class="posts-header">
 <h1 class="posts-title">Drafts<\/h1>
-<button class="btn-new-post" onclick="openNewPost()">+ New Post<\/button>
+<button class="btn-new-post" onclick="newPost()">+ New Post<\/button>
 <\/div>
 <input type="text" class="search-input" placeholder="Search drafts..." oninput="filterDrafts(this.value)" style="margin-bottom:8px">
 <div class="posts-table" id="drafts-table"><\/div>
